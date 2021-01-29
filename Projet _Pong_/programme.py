70ab0328c802d231e2cfc4ea278035c906e76c40
Garal09 <hugo09.delarue@laposte.net>
2021-01-29 23:42:59 +0100

    création de gitignore

--- Projet _Pong_/programme.py
+++ Projet _Pong_/programme.py
@@ -8,19 +8,38 @@ Created on Thu Oct 29 15:51:21 2020
 
 import arcade
 import random
+import os
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
 
 SW = 1000
 SH = 650
-ST = "Blue Square"
+ST = "The Ultimate Pong Game"
 SpeedBarre = 20
 
 
+class InstructionView(arcade.View):
+    
+    def on_show(self):
+        arcade.set_background_color(arcade.csscolor.DARK_SLATE_BLUE)
 
-class MyGame(arcade.Window):
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("The Ultimate Pong Game", SW/2, SH/2, arcade.color.WHITE, font_size=50, anchor_x="center")
+        arcade.draw_text("press any key to continue", SW / 2, SH / 2-75,
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
+
+class GameView(arcade.View):
     
     def __init__(self):
         
-        super().__init__(SW, SH, ST)
+        super().__init__()
         
         self.barre1_list = None
         self.barre2_list = None
@@ -36,13 +55,21 @@ class MyGame(arcade.Window):
         self.limite_Sprite = None
         self.game_over = None
         self.game_change = None
+        self.total_time = 0.0
+        self.counter_time = 0.0
+        self.message = ""
+        self.Lscore = 0
+        self.Rscore = 0
+        self.limite_list_L = None
+        self.limite_list_R = None
+
 
         
 
-        
         arcade.set_background_color(arcade.csscolor.WHITE)
         
-        
+        self.limite_list_L = arcade.SpriteList()
+        self.limite_list_R = arcade.SpriteList()
         self.ball_list = arcade.SpriteList()
         self.ball_Sprite = arcade.Sprite("balle.png", 0.1)
         self.ball_Sprite.center_x = 500
@@ -64,12 +91,15 @@ class MyGame(arcade.Window):
             wall.center_x = -64
             wall.center_y = i
             self.limite_list.append(wall)
+            self.limite_list_L.append(wall)
             
         for i in range(0, 768, 128):
             wall = arcade.Sprite("wall.png",1)
             wall.center_x = 1064
             wall.center_y = i
             self.limite_list.append(wall)
+            self.limite_list_R.append(wall)
+
         
 
         for i in range(0, 1000, 128):
@@ -137,43 +167,51 @@ class MyGame(arcade.Window):
     def on_draw(self):
         
         arcade.start_render()
-        
+        arcade.draw_text(self.message, 500, 300, arcade.color.BLACK, 30)
+        arcade.draw_text(str(self.Lscore), 0, 600, arcade.color.BLACK, 30)
+        arcade.draw_text(str(self.Rscore), 970, 600, arcade.color.BLACK, 30)
         self.barre1_list.draw()
         self.barre2_list.draw()
         self.wall_list.draw()
         self.ball_list.draw()
-        
+
     def on_update(self, delta_time):
         
         self.physics_engine1.update()
         self.physics_engine2.update()
         self.ball_Sprite.update()
+        self.total_time += delta_time
+
             
-        if arcade.check_for_collision_with_list(self.ball_Sprite, self.limite_list):
-            self.game_change = True
-        if self.game_change != True :
-            if arcade.check_for_collision(self.ball_Sprite, self.barre1_Sprite):
-                self.ball_Sprite.change_x *= -1.2
-            elif arcade.check_for_collision(self.ball_Sprite, self.barre2_Sprite):
-                self.ball_Sprite.change_x *= -1.2
-            elif arcade.check_for_collision_with_list(self.ball_Sprite, self.wall_list):
-                self.ball_Sprite.change_y *= -1
-        else:
-            
-            self.ball_list = arcade.SpriteList()
-            self.ball_Sprite = arcade.Sprite("balle.png", 0.1)
-            self.ball_Sprite.center_x = 500
+        if arcade.check_for_collision_with_list(self.ball_Sprite, self.limite_list_L):
+            self.Rscore += 1
+#            self.counter_time = self.total_time
+#            self.message = str(int(self.counter_time))
             self.ball_Sprite.center_y = 300
-            self.ball_list.append(self.ball_Sprite)
+            self.ball_Sprite.center_x = 500
             self.ball_Sprite.change_x = random.randrange(1,3)
             self.ball_Sprite.change_y = random.randrange(1,3)
             
-        
-        
+        if arcade.check_for_collision_with_list(self.ball_Sprite, self.limite_list_R):
+            self.Lscore += 1
+            self.ball_Sprite.center_y = 300
+            self.ball_Sprite.center_x = 500
+            self.ball_Sprite.change_x = random.randrange(1,3)
+            self.ball_Sprite.change_y = random.randrange(1,3)
+            
+
+        elif arcade.check_for_collision(self.ball_Sprite, self.barre1_Sprite):
+                self.ball_Sprite.change_x *= -1.2
+        elif arcade.check_for_collision(self.ball_Sprite, self.barre2_Sprite):
+                self.ball_Sprite.change_x *= -1.2
+        elif arcade.check_for_collision_with_list(self.ball_Sprite, self.wall_list):
+                self.ball_Sprite.change_y *= -1
+                
 
 def main():
-    window = MyGame()
-    window.setup()
+    window = arcade.Window(SW,SH,ST)
+    start_view = InstructionView()
+    window.show_view(start_view)
     arcade.run()
 
 
